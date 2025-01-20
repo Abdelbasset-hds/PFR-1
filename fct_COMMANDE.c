4a81d2880e8d94e6d38dbdae8ac0a789c614bbd9
wassimwali <wassimwali700@gmail.com>
2025-01-20 21:54:32 +0100

    20-01-2025

--- fct_COMMANDE.c
+++ fct_COMMANDE.c
@@ -35,31 +35,35 @@ void COMMANDE(){
     char motCleTourner[4][10]={"tourner","retourner","tourne","retourne"};
     char motCleTournerDroite[4][17]={"tourner à droite","tourner a droite","tourne à droite","tourne a droite"};
     char motCleTournerGauche[4][17]={"tourner à gauche","tourner a gauche","tourne à gauche","tourne a gauche"};
-
+    int i,j;
     printf("Entrez votre commande: ");
     fgets(commande, sizeof(commande),stdin);
     commande[strcspn(commande, "\n")]='\0';
     int nbrmot=filtre(commande,list);
-    fopen("C:\\commandes.txt","a");
-    for (int i = 0; i < nbrmot; i++){
-        for (int j=0;j<11;j++){
+    commandes = fopen("commandes.txt", "a");
+    if (commandes == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return; // Arrête la fonction si le fichier n'a pas pu être ouvert
+    }
+    for ( i = 0; i < nbrmot; i++){
+        for ( j=0;j<11;j++){
             if ((strcmp(list[i], motCleAvancer[j]) == 0)) {
                 fprintf(commandes,"%s ,","avance");
-                if (estEntier(list[i+1])) {
+                if (i + 1 < nbrmot && estEntier(list[i+1])) {
                     fprintf(commandes,"%s ,",list[i+1]);
                 }
-                else if (estEntier(list[i+2])) {
+                else if (i + 2 < nbrmot && estEntier(list[i+2])) {
                     fprintf(commandes,"%s ,",list[i+2]);
                 }
             }
         }
-        for (int j=0;j<4;j++){
+        for ( j=0;j<4;j++){
             if ((strcmp(list[i], motCleTourner[j]) == 0)) {
                 fprintf(commandes,"%s ,","tourne");
-                if (estEntier(list[i+1])) {
+                if (i + 1 < nbrmot && estEntier(list[i+1])) {
                     fprintf(commandes,"%s ,",list[i+1]);
                 }
-                else if (estEntier(list[i+2])) {
+                else if (i + 2 < nbrmot && estEntier(list[i+2])) {
                     fprintf(commandes,"%s ,",list[i+2]);
                 }
                 else {
@@ -67,13 +71,13 @@ void COMMANDE(){
                 }
             }
         }
-        for (int j=0;j<4;j++){
+        for ( j=0;j<4;j++){
             if ((strcmp(list[i], motCleTournerGauche[j]) == 0)) {
                 fprintf(commandes,"%s ,","tourne_gch");
-                if (estEntier(list[i+1])) {
+                if (i + 1 < nbrmot && estEntier(list[i+1])) {
                     fprintf(commandes,"%s ,",list[i+1]);
                 }
-                else if (estEntier(list[i+2])) {
+                else if (i + 2 < nbrmot && estEntier(list[i+2])) {
                     fprintf(commandes,"%s ,",list[i+2]);
                 }
                 else {
@@ -81,13 +85,13 @@ void COMMANDE(){
                 }
             }
         }
-        for (int j=0;j<4;j++){
+        for ( j=0;j<4;j++){
             if ((strcmp(list[i], motCleTournerDroite[j]) == 0)) {
                 fprintf(commandes,"%s ,","tourne_dr");
-                if (estEntier(list[i+1])) {
+                if (i + 1 < nbrmot && estEntier(list[i+1])) {
                     fprintf(commandes,"%s ,",list[i+1]);
                 }
-                else if (estEntier(list[i+2])) {
+                else if (i + 2 < nbrmot && estEntier(list[i+2])) {
                     fprintf(commandes,"%s ,",list[i+2]);
                 }
                 else {
@@ -95,8 +99,8 @@ void COMMANDE(){
                 }
             }
         }
-        
-        
+
+
     }
     fclose(commandes);
 }
